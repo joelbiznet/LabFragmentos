865e810ab6a2ae34214ff440fd01196d42f217a3
joelbiznet <gerencia@idw.com.pe>
2019-08-26 20:45:28 -0500

    Nuevo2

--- app/src/main/java/com/example/labfragmentos/MainActivity.java
+++ app/src/main/java/com/example/labfragmentos/MainActivity.java
@@ -10,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ParrafoFragment parrafoFragment = new ParrafoFragment();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container,parrafoFragment)
+                .commit();
     }
 }
